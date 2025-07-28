d24f18d47355c526b9472e91774309efab89338e
Danil <147924429+MajesticNthg@users.noreply.github.com>
2025-07-29 00:44:35 +1000

    create project 4. aBST

---
+++ 4. aBST/aBST.java
@@ -0,0 +1,95 @@
+import java.util.*;
+
+class aBST {
+    public Integer Tree [];
+
+    public aBST (int depth) {
+        int tree_size = calculateDepth(depth);
+        Tree = new Integer[tree_size];
+        for (int i = 0; i < tree_size; i++) {
+            Tree[i] = null;
+        }
+    }
+
+    private int calculateDepth (int depth) {
+        if (depth == 0) {
+            return 0;
+        }
+
+        if (depth == 1) {
+            return 1;
+        }
+
+        int result_depth = 1;
+
+        for (int i = 0; i < depth - 2; i++) {
+            result_depth = result_depth * 2 + 1;
+        }
+
+        return result_depth;
+    }
+
+    public Integer FindKeyIndex (int key) {
+        if (Tree.length == 0) {
+            return null;
+        }
+
+        int i = 0;
+        while (i < Tree.length) {
+            if (Tree[i] == null) {
+                return -i;
+            }
+            if (Tree[i] == key) {
+                return i;
+            }
+            if (Tree[i] < key) {
+                i = 2 * i + 2;
+                continue;
+            }
+            if (Tree[i] > key) {
+                i = 2 * i + 1;
+            }
+        }
+        return null;
+    }
+
+    public int AddKey (int key) {
+        int i = 0;
+        while (i < Tree.length) {
+            if (Tree[i] == null) {
+                Tree[i] = key;
+                return i;
+            }
+            if (Tree[i] == key) {
+                return i;
+            }
+            if (Tree[i] < key) {
+                i = 2 * i + 2;
+                continue;
+            }
+            if (Tree[i] > key) {
+                i = 2 * i + 1;
+            }
+        }
+        return -1;
+    }
+
+    public ArrayList<Integer> WideAllNodes() {
+        ArrayList<Integer> WideAllNodes = new ArrayList<>();
+
+        if (Tree.length == 0) {
+            return WideAllNodes;
+        }
+
+        int i = 0;
+        while (i < Tree.length) {
+            if (Tree[i] != null) {
+                WideAllNodes.add(Tree[i]);
+            }
+            i++;
+        }
+
+        return WideAllNodes;
+    }
+
+}
---
+++ 4. aBST/aBSTTest.java
@@ -0,0 +1,81 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class aBSTTest {
+
+    aBST tree;
+
+    @BeforeEach
+    void setUp () {
+        tree = new aBST(4);
+    }
+
+    @Test
+    void testAddKey() {
+        tree.AddKey(4);
+        tree.AddKey(3);
+        tree.AddKey(5);
+        tree.AddKey(6);
+
+        ArrayList<Integer> testWideAllNodes = new ArrayList<>();
+        testWideAllNodes = tree.WideAllNodes();
+        assertEquals(4, testWideAllNodes.size());
+    }
+
+    @Test
+    void testWideAllNodes() {
+        tree.AddKey(10);
+        tree.AddKey(5);
+        tree.AddKey(3);
+        tree.AddKey(8);
+        tree.AddKey(15);
+        tree.AddKey(13);
+        tree.AddKey(18);
+
+        ArrayList<Integer> testWideAllNodes = new ArrayList<>();
+        testWideAllNodes = tree.WideAllNodes();
+        assertEquals(7, testWideAllNodes.size());
+        assertEquals(testWideAllNodes.getFirst(), 10);
+        assertEquals(testWideAllNodes.getLast(), 18);
+        assertEquals(testWideAllNodes.get(1), 5);
+        assertEquals(testWideAllNodes.get(2), 15);
+        assertEquals(testWideAllNodes.get(3), 3);
+    }
+
+    @Test
+    void testFindKeyIndex() {
+        tree.AddKey(10);
+        tree.AddKey(5);
+        tree.AddKey(3);
+        tree.AddKey(8);
+        tree.AddKey(15);
+        tree.AddKey(13);
+
+
+        assertEquals(tree.FindKeyIndex(10), 0);
+        assertNull(tree.FindKeyIndex(11));
+        assertEquals(tree.FindKeyIndex(18), -6);
+    }
+
+    @Test
+    void testEmptyTree() {
+        aBST testTree = tree = new aBST(0);
+        assertNull(tree.FindKeyIndex(11));
+    }
+
+    @Test
+    void testNotFindIndex() {
+        tree.AddKey(10);
+        tree.AddKey(5);
+        tree.AddKey(3);
+        tree.AddKey(8);
+        tree.AddKey(15);
+        tree.AddKey(13);
+
+        assertNull(tree.FindKeyIndex(11));
+    }
+}
