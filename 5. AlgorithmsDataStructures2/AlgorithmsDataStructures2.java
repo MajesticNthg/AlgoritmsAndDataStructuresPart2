bc9b0400eb6e7f524f6212784e392eaf91872b0d
Danil <147924429+MajesticNthg@users.noreply.github.com>
2025-08-08 22:22:18 +1000

    Update AlgorithmsDataStructures2.java
    Change int[] BalancedBST on Integer[]BalancedBST for BalancedBST consistend of null. After that - cteate new int[] BalancedBST correct size and without null

--- 5. AlgorithmsDataStructures2/AlgorithmsDataStructures2.java
+++ 5. AlgorithmsDataStructures2/AlgorithmsDataStructures2.java
@@ -6,15 +6,15 @@ public class AlgorithmsDataStructures2 {
         Arrays.sort(isSorted);
         int height = calculateDepth(isSorted.length);
         int size = (int) (Math.pow(2, height + 1) - 1);
-        int[] BalancedBST = new int[size];
+        Integer[] BalancedBST = new Integer[size];
         int indexBalancedBST = 0;
 
         return createBalancedBST(BalancedBST, indexBalancedBST, isSorted, 0, isSorted.length - 1);
     }
 
-    private static int[] createBalancedBST (int[] BalancedBST, int indexBalancedBST, int[] isSorted, int leftSubTree, int rightSubTree) {
+    private static int[] createBalancedBST (Integer[] BalancedBST, int indexBalancedBST, int[] isSorted, int leftSubTree, int rightSubTree) {
         if (leftSubTree > rightSubTree) {
-            return BalancedBST;
+            return createFinalBalancedBST(BalancedBST);
         }
 
         int mid = (leftSubTree + rightSubTree) / 2;
@@ -23,7 +23,7 @@ public class AlgorithmsDataStructures2 {
         createBalancedBST(BalancedBST, 2 * indexBalancedBST + 1, isSorted, leftSubTree, mid - 1);
         createBalancedBST(BalancedBST, 2 * indexBalancedBST + 2, isSorted, mid + 1, rightSubTree);
 
-        return BalancedBST;
+        return createFinalBalancedBST(BalancedBST);
     }
 
     private static int calculateDepth (int isSortedSize) {
@@ -41,5 +41,30 @@ public class AlgorithmsDataStructures2 {
         }
 
         return result_depth;
+    }
+
+    private static int[] createFinalBalancedBST (Integer[] BalancedBST) {
+        int[] finalBalancedBST = new int[calculateMassiveSize(BalancedBST)];
+
+        int indexBalancedBST = 0;
+        for (Integer integer : BalancedBST) {
+            if (integer != null) {
+                finalBalancedBST[indexBalancedBST] = integer;
+                indexBalancedBST++;
+            }
+        }
+
+        return finalBalancedBST;
+    }
+
+    private static int calculateMassiveSize(Integer[] BalancedBST) {
+        int size = 0;
+
+        for (Integer integer : BalancedBST) {
+            if (integer != null) {
+                size++;
+            }
+        }
+        return size;
     }
 }
