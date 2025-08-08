47cb69e85fb99caca1ecd100d7a5ae595e87bda8
Danil <147924429+MajesticNthg@users.noreply.github.com>
2025-08-08 20:42:24 +1000

    Update AlgorithmsDataStructures2.java
    Add function 'calculateDepth' change method 'createBalancedBST' (use index instead submassive)

--- 5. AlgorithmsDataStructures2/AlgorithmsDataStructures2.java
+++ 5. AlgorithmsDataStructures2/AlgorithmsDataStructures2.java
@@ -1,36 +1,45 @@
 import java.util.*;
 
-
 public class AlgorithmsDataStructures2 {
     public static int[] GenerateBBSTArray (int[] a) {
         int[] isSorted = Arrays.copyOf(a, a.length);
         Arrays.sort(isSorted);
+        int height = calculateDepth(isSorted.length);
+        int size = (int) (Math.pow(2, height + 1) - 1);
+        int[] BalancedBST = new int[size];
+        int indexBalancedBST = 0;
 
-        int[] BalancedBST = new int[isSorted.length];
-        BalancedBST[0] = isSorted[isSorted.length / 2];
-
-        int indexBalancedBST = 1;
-        return createBalancedBST(BalancedBST, Arrays.copyOfRange(isSorted, 0, isSorted.length / 2), Arrays.copyOfRange(isSorted, isSorted.length / 2, isSorted.length), indexBalancedBST);
+        return createBalancedBST(BalancedBST, indexBalancedBST, isSorted, 0, isSorted.length - 1);
     }
 
-    private static int[] createBalancedBST (int[] BalancedBST, int[] left, int[] right, int indexBalancedBST) {
-        if (indexBalancedBST > BalancedBST.length || (left.length <= 1 || right.length <= 1 )) {
+    private static int[] createBalancedBST (int[] BalancedBST, int indexBalancedBST, int[] isSorted, int leftSubTree, int rightSubTree) {
+        if (leftSubTree > rightSubTree) {
             return BalancedBST;
         }
 
+        int mid = (leftSubTree + rightSubTree) / 2;
+        BalancedBST[indexBalancedBST] = isSorted[mid];
 
-        BalancedBST[indexBalancedBST] = searchLeftChild(Arrays.copyOfRange(left, 0, left.length / 2));
-        BalancedBST[indexBalancedBST + 1] = searchRightChild(Arrays.copyOfRange(right, right.length / 2, right.length));
+        createBalancedBST(BalancedBST, 2 * indexBalancedBST + 1, isSorted, leftSubTree, mid - 1);
+        createBalancedBST(BalancedBST, 2 * indexBalancedBST + 2, isSorted, mid + 1, rightSubTree);
 
-        createBalancedBST(BalancedBST, Arrays.copyOfRange(left, 0, left.length / 2), Arrays.copyOfRange(left, left.length / 2, left.length), indexBalancedBST + 2);
-        createBalancedBST(BalancedBST, Arrays.copyOfRange(right, 0, right.length / 2), Arrays.copyOfRange(right, right.length / 2, right.length), indexBalancedBST + 2);
         return BalancedBST;
     }
-    private static int searchLeftChild (int[] isSorted) {
-        return isSorted[isSorted.length / 2];
-    }
 
-    private static int searchRightChild (int[] isSorted) {
-        return isSorted[isSorted.length / 2];
+    private static int calculateDepth (int isSortedSize) {
+        if (isSortedSize <= 0) {
+            return 0;
+        }
+
+        int result_depth = 0;
+
+        for (int i = 0; i < isSortedSize; i++) {
+            result_depth = result_depth * 2 + 1;
+            if (result_depth > isSortedSize) {
+                return i;
+            }
+        }
+
+        return result_depth;
     }
 }
