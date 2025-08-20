a5693ab02903ef098961cdfbe7bfef471baf15fa
Danil <147924429+MajesticNthg@users.noreply.github.com>
2025-08-21 04:05:28 +1000

    Update BalancedBST-3.java
    Add new tests for check balanced

--- 6. Building balanced binary search trees/BalancedBST-3.java
+++ 6. Building balanced binary search trees/BalancedBST-3.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+
 class BalancedBSTTest {
 
     BalancedBST tree;
@@ -21,5 +22,61 @@ class BalancedBSTTest {
         assertEquals(1, tree.Root.LeftChild.Level);
         assertEquals(1, tree.Root.RightChild.Level);
         assertEquals(2, tree.Root.RightChild.RightChild.Level);
+    }
+
+    @Test
+    void isBalancedFirst() {
+        tree.GenerateTree(massiveTree);
+        assertTrue(tree.IsBalanced(tree.Root));
+    }
+
+    @Test
+    void isBalancedSecond() {
+        BSTNode root = new BSTNode(10, null);
+        root.LeftChild = new BSTNode(5, root);
+        root.RightChild = new BSTNode(15, root);
+
+        root.LeftChild.LeftChild = new BSTNode(2, root.LeftChild);
+        root.LeftChild.RightChild = new BSTNode(8, root.LeftChild);
+
+        root.LeftChild.LeftChild.LeftChild = new BSTNode(1, root.LeftChild.LeftChild);
+        root.LeftChild.LeftChild.RightChild = new BSTNode(3, root.LeftChild.LeftChild);
+
+        assertFalse(tree.IsBalanced(root));
+    }
+
+    @Test
+    void isBalancedThird() {
+        BSTNode root = new BSTNode(10, null);
+        root.LeftChild = new BSTNode(5, root);
+        root.RightChild = new BSTNode(15, root);
+
+        root.LeftChild.LeftChild = new BSTNode(2, root.LeftChild);
+        root.LeftChild.RightChild = new BSTNode(8, root.LeftChild);
+
+        root.LeftChild.LeftChild.LeftChild = new BSTNode(1, root.LeftChild.LeftChild);
+
+        assertFalse(tree.IsBalanced(root));
+    }
+
+    @Test
+    void isBalancedFourth() {
+        BSTNode root = new BSTNode(10, null);
+        root.LeftChild = new BSTNode(5, null);
+        root.LeftChild.LeftChild = new BSTNode(2, null);
+        root.LeftChild.LeftChild.LeftChild = new BSTNode(1, null);
+        root.RightChild = new BSTNode(15, null);
+        root.RightChild.RightChild = new BSTNode(20, null);
+
+        assertFalse(tree.IsBalanced(root));
+    }
+
+    @Test
+    void isBalancedFifth() {
+        BSTNode root2 = new BSTNode(10, null);
+        root2.LeftChild = new BSTNode(5, null);
+        root2.LeftChild.LeftChild = new BSTNode(2, null);
+
+        assertFalse(tree.IsBalanced(root2));
     }
 }
