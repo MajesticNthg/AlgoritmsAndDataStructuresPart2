7f390c128aa74bbdecfec80eeb4ba5c797a2fbe8
Danil <147924429+MajesticNthg@users.noreply.github.com>
2025-10-16 21:27:31 +1000

    Update and rename countTriangle.java to GraphWithTriangle-2.java
    rename file

---
+++ 12. GraphWithTriangle/GraphWithTriangle-2.java
@@ -0,0 +1,42 @@
+public class countTriangle {
+    private final int max_vertex;
+    private final Vertex[] vertex;
+    private final int[][] m_adjacency;
+
+    public countTriangle(int max_vertex, Vertex[] vertex, int[][] m_adjacency) {
+        this.max_vertex = max_vertex;
+        this.vertex = vertex;
+        this.m_adjacency = m_adjacency;
+    }
+        public int countTriangle() {
+            return calculateCountTriangle(0, 0, 1);
+        }
+
+        private int calculateCountTriangle(int countTriangle, int i, int j) {
+            if (i >= max_vertex) {
+                return countTriangle;
+            }
+
+            if (j >= max_vertex) {
+                return calculateCountTriangle(countTriangle, i + 1, i + 2);
+            }
+
+            if (vertex[i] != null && vertex[j] != null && m_adjacency[i][j] == 1) {
+                countTriangle = isTriangle(i, j, countTriangle);
+            }
+
+            return calculateCountTriangle(countTriangle, i, j + 1);
+        }
+
+        private int isTriangle(int i, int j, int countTriangle) {
+            for (int x = j + 1; x < max_vertex; x++) {
+                if (vertex[x] == null) continue;
+                if (m_adjacency[i][x] == 1 && m_adjacency[j][x] == 1) {
+                    countTriangle++;
+                }
+            }
+            return countTriangle;
+        }
+    }
+
+
